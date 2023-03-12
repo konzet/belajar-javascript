9cd0be05ecd9dd34fa8aaa38d1c4732c449b3ce7
ramdani <konzet@github.com>
2023-03-12 12:02:35 +0700

    perojek belajar javascript

---
+++ main.js
@@ -0,0 +1,4 @@
+let saldoAwal = prompt ('berapa saldo awal kamu?')
+let saldoTambah = prompt ('bearapa saldo akhir kamu?')
+const saldoAkhir = saldoAwal+saldoTambah
+alert(`jadi saldo akhirkamu adalah Rp.${saldoAkhir}`)
